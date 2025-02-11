bec48ed02974b7bbe12924a9b5303863daf61280
RaulLopez3108 <raul_leonardo20@outlook.es>
2025-02-10 19:30:17 -0700

    version final

--- bosque_aleatorio.py
+++ bosque_aleatorio.py
@@ -1,42 +1,37 @@
-
 import random
-import arboles_numericos
+import arboles_numericos as an
+from collections import Counter
+__author__ = "Raul Lopez"
+__date__ = "enero 2025"
+
 
 def entrena_bosque(datos,target,clase_default,
-                   max_profundidad=None,n_arboles=100,
-                   acc_nodo=1.0,min_ejemplos=0,frac_datos=0.8,
-                   frac_variables=0.8):
+                   max_profundidad=None,n_arboles=100,  
+                   variables_seleccionadas = None):
     bosque = []
+    random.seed(42)
+    subconjunto = random.choices(datos, k = len(datos))
 
-    for _ in range(n_arboles):
-        datos_arbol = datos.sample(frac= frac_datos,replace = True)
 
-        variables_seleccionadas = random.sample(
-            list(datos_arbol.columns),
-            int(frac_variables * len(datos_arbol.columns))
-        )
-
-        arbol = arboles_numericos.entrena_arbol(
-            datos_arbol[variables_seleccionadas],
-            target,
-            clase_default,
-            max_profundidad=max_profundidad,
-            acc_nodo=acc_nodo,
-            min_ejemplos=min_ejemplos,
+    for _ in range(n_arboles):               
+        arbol = an.entrena_arbol(
+            datos = subconjunto,
+            target = target,
+            clase_default = clase_default,
+            max_profundidad=max_profundidad,            
             variables_seleccionadas=variables_seleccionadas
         )
         bosque.append(arbol)
     return bosque
 
-def predice_bosque(bosque,datos):
-    return[bosque.predice(d)for d in datos]
 
+def predice_bosque(bosque, datos):
+    predicciones = []
+    for arbol in bosque:
+        predicciones.append(an.predice_arbol(arbol, datos))
+    return Counter(predicciones).most_common(1)[0][0]
 
-def predice():
-    if arboles_numericos.self.terminal:
-            return arboles_numericos.self.clase_default               
-    if arboles_numericos.instancia[arboles_numericos.self.atributo] < arboles_numericos.self.valor:
-        return arboles_numericos.self.hijo_menor.predice(arboles_numericos.instancia)       
-    return arboles_numericos.self.hijo_mayor.predice(arboles_numericos.instancia)
-
+def evaula_bosque(bosque,datos,target):
+    predicciones = predice_bosque(bosque, datos)
+    return sum(1 for p, d in zip(predicciones, datos) if p == d[target]) / len(datos)
 
